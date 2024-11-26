8839cf381621b4c5a2a7b544c65c3ac956475716
titch <sujinlee0202@gmail.com>
2024-11-26 18:26:02 +0900

    feat: post 컴포넌트 UI 추가

--- src/app/(content)/home/HomePage.tsx
+++ src/app/(content)/home/HomePage.tsx
@@ -1,4 +1,12 @@
+import CreatePostButton from "@/app/components/CreatePostButton";
+import Post from "@/app/components/Post";
+
 const HomePage = () => {
-  return <div className="font-pretendard">Hello World!</div>;
+  return (
+    <div className="mr-5 flex flex-col gap-10 pt-5">
+      <CreatePostButton />
+      <Post />
+    </div>
+  );
 };
 export default HomePage;
