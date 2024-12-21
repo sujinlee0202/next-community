f7dc10ec7ee76732512a4d85a5b860e441400b97
titch <sujinlee0202@gmail.com>
2024-12-21 18:22:44 +0900

    feat: 게시물 더보기 기능 추가

--- src/app/components/Post.tsx
+++ src/app/components/Post.tsx
@@ -10,9 +10,18 @@ import dayjs from "dayjs";
 
 const Post = ({ post }: { post: PostType }) => {
   const [isLike, setIsLike] = useState(false);
+  const [isExpanded, setIsExpanded] = useState(false);
+  const sentences = post.content.split(/\n/);
+  const displayedText = isExpanded
+    ? post.content
+    : sentences.slice(0, 3).join("\n") + (sentences.length > 3 ? "..." : "");
 
   const handleIsLike = () => {
     setIsLike((prev) => !prev);
+  };
+
+  const handleExpanded = () => {
+    setIsExpanded(true);
   };
 
   return (
@@ -33,8 +42,12 @@ const Post = ({ post }: { post: PostType }) => {
       <p className="py-5 font-bold">{post.title}</p>
 
       <pre className="relative mb-5 whitespace-pre-wrap font-pretendard">
-        {post.content}
-        <button className="text-gray-600">...더보기</button>
+        {displayedText}
+        {sentences.length > 3 && !isExpanded && (
+          <button onClick={handleExpanded} className="text-gray-600">
+            더보기
+          </button>
+        )}
       </pre>
 
       <div className="flex gap-3 text-sm font-bold text-slate-600">
